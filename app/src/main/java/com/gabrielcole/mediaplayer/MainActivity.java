43868acfd3fc75e0f3de2b8a7c90c8208bcc53a7
Gabriel Cole <lgcw92@gmail.com>
2016-09-17 19:35:24 -0500

    play and stop button added to the project

--- app/src/main/java/com/gabrielcole/mediaplayer/MainActivity.java
+++ app/src/main/java/com/gabrielcole/mediaplayer/MainActivity.java
@@ -6,12 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button mPlayButton;
-    private Button mStopButton;
-    private Button mResetButton;
+
+    private ImageView mPlayIcon;
+    private ImageView mPauseIcon;
+    private ImageView mStopIcon;
 
     private MediaPlayer mMediaPlayer;
     @Override
@@ -21,27 +23,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mMediaPlayer = MediaPlayer.create(this, R.raw.no_romeo);
 
-        mPlayButton = (Button)findViewById(R.id.play_button);
-        mPlayButton.setOnClickListener(this);
-        mStopButton = (Button)findViewById(R.id.stop_button);
-        mStopButton.setOnClickListener(this);
-        mResetButton = (Button)findViewById(R.id.reset_button);
-        mResetButton.setOnClickListener(this);
-
+        mPlayIcon = (ImageView)findViewById(R.id.play_icon);
+        mPlayIcon.setOnClickListener(this);
+        mPauseIcon = (ImageView)findViewById(R.id.pause_icon);
+        mPauseIcon.setOnClickListener(this);
+        mStopIcon = (ImageView)findViewById(R.id.stop_icon);
+        mStopIcon.setOnClickListener(this);
+        mPauseIcon.setVisibility(View.INVISIBLE);
     }
 
     @Override
     public void onClick(View view){
         switch(view.getId()){
-            case R.id.play_button:
+            case R.id.play_icon:
                 mMediaPlayer.start();
-                Log.v("PLAYER", mMediaPlayer.getDuration() + "");
+                mPlayIcon.setVisibility(View.INVISIBLE);
+                mPauseIcon.setVisibility(View.VISIBLE);
                 break;
-            case R.id.stop_button:
+            case R.id.pause_icon:
                 mMediaPlayer.pause();
+                mPauseIcon.setVisibility(View.INVISIBLE);
+                mPlayIcon.setVisibility(View.VISIBLE);
                 break;
-            case R.id.reset_button:
+            case R.id.stop_icon:
                 mMediaPlayer.reset();
+                mPlayIcon.setVisibility(View.VISIBLE);
+                mPauseIcon.setVisibility(View.INVISIBLE);
                 mMediaPlayer = MediaPlayer.create(this, R.raw.no_romeo);
                 break;
         }
