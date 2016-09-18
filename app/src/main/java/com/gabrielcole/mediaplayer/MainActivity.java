64e477432cd56412ce88a9b2138adea36969dbe0
Gabriel Cole <lgcw92@gmail.com>
2016-09-17 19:01:51 -0500

    play and stop button added to the project

--- app/src/main/java/com/gabrielcole/mediaplayer/MainActivity.java
+++ app/src/main/java/com/gabrielcole/mediaplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.gabrielcole.mediaplayer;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,8 +22,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mMediaPlayer = MediaPlayer.create(this, R.raw.no_romeo);
 
         mPlayButton = (Button)findViewById(R.id.play_button);
+        mPlayButton.setOnClickListener(this);
         mStopButton = (Button)findViewById(R.id.stop_button);
+        mStopButton.setOnClickListener(this);
         mResetButton = (Button)findViewById(R.id.reset_button);
+        mResetButton.setOnClickListener(this);
 
     }
 
@@ -30,6 +34,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view){
         switch(view.getId()){
             case R.id.play_button:
+                mMediaPlayer.start();
+                Log.v("PLAYER", mMediaPlayer.getDuration() + "");
+                break;
+            case R.id.stop_button:
+                mMediaPlayer.pause();
+                break;
+            case R.id.reset_button:
+                mMediaPlayer.reset();
+                mMediaPlayer = MediaPlayer.create(this, R.raw.no_romeo);
+                break;
         }
     }
 }
